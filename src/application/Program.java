87211684a6823ddb57d980768523255ef5e689b4
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-01-05 20:02:40 -0300

    remove todos os arquivos que são maiores do que 3 caracteres

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,8 +14,8 @@ public class Program {
 		// TreeSet ordena os dados por ordem alfabetica
 		// TV esta na frente porque o V é maiusulo é tem prioridade o Maiusculo sobre o Minusculo
 		
-		set.remove("Table");
-		// Remove "Table" da lista		
+		set.removeIf(x -> x.length() >= 3); // remover todos que tenham 3 elementos maiores ou iguais a 3
+		
 		for(String p : set) {
 			System.out.println(p);
 		}
