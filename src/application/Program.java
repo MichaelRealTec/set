313bdb10ec89d13556ec711a216ac338d988783a
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-01-05 20:19:12 -0300

    usando os comandos união, intercessão e diferença do TreeSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,35 @@
 package application;
 
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Set<String> set = new TreeSet<>();
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 		
-		set.add("TV");
-		set.add("Tablet"); 
-		set.add("Notebook");
-		// TreeSet ordena os dados por ordem alfabetica
-		// TV esta na frente porque o V é maiusulo é tem prioridade o Maiusculo sobre o Minusculo
+		System.out.println("Conjunto a: " + a);
+		System.out.println("--------------------------------");
+		System.out.println("Conjunto b: " + b);
+		System.out.println("================================");
 		
-		set.removeIf(x -> x.charAt(0) == 'T'); // remover todos que tem a primeira letra 'T' 
+		// Union ou união
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.
+		println("Union: " + c);
 		
-		for(String p : set) {
-			System.out.println(p);
-		}
+		// Intersection ou interceção
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println("Intersection: " + d);
+		
+		// Difference ou diferença
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println("Difference: " + e);
 		
 
 	}
